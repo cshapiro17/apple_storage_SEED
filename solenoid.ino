7e56d2fe189f0ee5651db9f2b9dc0392533f2a84
raythibeault1 <raythibeault1@gmail.com>
2023-09-29 13:19:50 -0400

    Adding text to solenoid.ino

--- solenoid.ino
+++ solenoid.ino
@@ -1,4 +1,5 @@
-//this is for testing the mimciking of the solenoid unit
+//this is for testing the mimciking of the solenoid unit opening and closing
+
 void setup() {
   // put your setup code here, to run once:
 
