97605e6759c80a088d84d68a06d44df74acf9549
raythibeault1 <raythibeault1@gmail.com>
2023-10-03 18:31:06 -0400

    Working potentiometer program w/ state machine and LEDs

--- potentiometer.ino
+++ potentiometer.ino
@@ -7,9 +7,9 @@
 
 static unsigned int state;
 static unsigned long time;
-int LED1 = 5;
-int LED2 = 6;
-int potent = A3; //placeholder
+int LED1 = 5;                 //LED1 digital output pin
+int LED2 = 6;                 //LED2 digital output pin
+int potent = A3;              //input analog pin for potentiometer
 int potValue = 0;
 boolean solenoid;
 
@@ -17,9 +17,7 @@ void setup() {
   // pin classifications
   pinMode(LED1,OUTPUT);
   pinMode(LED2,OUTPUT);
-
   Serial.begin(9600); //Initialize serial communication with a baud rate of 9600
-  time = 0;
   state = 1;
 }
 
@@ -28,23 +26,25 @@ void loop() {
   switch(state){
     //Restart State
     case 1:
-      state = 2;
       Serial.println(state);
+      state = 2;
   
       break;
     
     //Sense State
     case 2:
       delay(1000);
+      Serial.println(state);
       potValue = analogRead(potent);
+      //Serial.println(potValue);
       state = 3;
 
       break;
     
     //Log value State
     case 3:
-      Serial.println("Potentiometer Value: " + potValue);
-      delay(1000);
+      Serial.println(potValue);
+      //delay(1000);
       state = 4;
 
 
@@ -53,18 +53,18 @@ void loop() {
     //Evaluate Value State
     case 4:
       Serial.println(state);
-      if(potValue>1000){
+      if(potValue>700){
         digitalWrite(LED1,HIGH);
         digitalWrite(LED2,LOW);
         solenoid = true;
       }
-      else if(potValue<100){
+      else if(potValue<400){
         digitalWrite(LED1,LOW);
         digitalWrite(LED2,HIGH);
         solenoid = true;
       }
       else{
-        digitalWrite(LED1,LOW);
+        digitalWrite(LED1,HIGH);
         digitalWrite(LED2,HIGH);
         solenoid = false;
       }
