9f2991c113f3f74dff6dd63acb795bc680f3fe2f
raythibeault1 <raythibeault1@gmail.com>
2023-10-05 10:55:17 -0400

    updated potentiometer.ino to work in VSCode

---
+++ Potentiometer/Potentiometer.ino
@@ -0,0 +1,145 @@
+//This code is for testing the potentiometer and logging into to the console. Two LEDs are used, turning on for different potentiometer
+//input values. Pins and values can be changed as this is preliminary code
+
+
+//#include "Arduino.h"
+//#include "millisDelay.h"
+
+static unsigned int state;
+static unsigned long time;
+int LED1 = 5;                 //LED1 digital output pin
+int LED2 = 6;                 //LED2 digital output pin
+int LED3;
+int LED4;
+int potent1 = A0;              //input analog pin for potentiometer
+int potent2 = A1;
+int solenoidPin1 = 4;          //solenoid input pin
+int room1;
+int room2;
+int highLevel;                //high level for sensing
+int lowLevel;                 //low level for sensing
+boolean solenoid1;
+boolean solenoid2;
+int potValue1;                 //analog value for potentiometer 1
+int potValue2;                 //analog value for potentiometer 2
+
+void setup() {
+  // pin classifications
+  pinMode(LED1,OUTPUT);
+  pinMode(LED2,OUTPUT);
+  Serial.begin(9600); //Initialize serial communication with a baud rate of 9600
+  state = 1;
+}
+
+void loop() {
+  // Initial stuff
+  switch(state){
+    //Restart State
+    case 1:
+      Serial.println("Startup");
+      Serial.println("Room/tValue");    
+//      Serial.println("High Margin: ");
+//      while (Serial.available() == 0) {
+//      }
+//      highLevel = Serial.parseInt();
+//      delay(1000);
+//      
+//      Serial.println("Low Margin: ");
+//      while (Serial.available() == 0 && newData == false) {
+//        Serial.println("1");
+//        newData = true;
+//      }
+//      lowLevel = Serial.parseInt();
+
+      highLevel = 600;
+      lowLevel = 300;
+      room1 = 1;
+      room2 = 2;
+      delay(1000);
+      state = 2;
+  
+      break;
+    
+    //Sense State
+    case 2:
+      delay(500);
+      Serial.println("Reading Potentiometer Value:");
+      potValue1 = analogRead(potent1);
+      //potValue2 = analogRead(potent2);
+      //Serial.println(potValue);
+      state = 3;
+      break;
+    
+    //Log value State
+    case 3:
+      //Serial.println(potValue1);
+      //delay(1000);
+      logValue(room1,potValue1);
+      logValue(room2,potValue2);
+      state = 4;
+      break;
+    
+    //Evaluate Value State
+    case 4:
+      solenoid1 = evaluateSensor(potValue1,highLevel,lowLevel,LED1,LED2);
+      //solenoid2 = evaluateSensor(potValue2,highLevel,lowLevel,LED3,LED4);      
+      state = 5;
+      break;
+    
+    //Solenoid mimicking state
+    case 5:      
+      solenoidChange(solenoid1,room1,solenoidPin1);
+      //solenoidChange(solenoid2,room2,solenoidPin2);
+      state = 2;
+      break;
+  }
+}
+
+
+void logValue(int room, int potValue){
+  Serial.print(room);
+  Serial.print("/t");
+  Serial.print(potValue);
+}
+
+
+//this function takes in a boolean to see if the solenoid should be open or closed, which room to mimick, and which pin that solenoid changes.
+void solenoidChange(boolean solenoid,int room, int solenoidPin){
+  if(solenoid == true){
+        digitalWrite(solenoidPin,HIGH);
+        Serial.print("Solenoid Open in room ");
+        Serial.println(room);
+      }
+  else{
+    digitalWrite(solenoidPin,LOW);
+    Serial.print("Solenoid Closed in room ");
+        Serial.println(room);
+  }
+  delay(500);
+}
+
+/*evaluateSensor is used to measure the value of the potentiometer and determine if a
+ * solenoid should be changed or not.
+ * Inputs: potentiometer value, high level threshold, low level threshold, 2 LEDs to mimick
+ * Output: Boolean true or false to mimick solenoid 
+ */
+boolean evaluateSensor(int potValue, int highLevel, int lowLevel, int LEDA, int LEDB){
+  boolean solenoid;
+  Serial.println("Evaluating Potentiometer Value");
+  if(potValue>highLevel){
+    digitalWrite(LEDA,HIGH);
+    digitalWrite(LEDB,LOW);
+    solenoid = true;
+  }
+  else if(potValue<lowLevel){
+    digitalWrite(LEDA,LOW);
+    digitalWrite(LEDB,HIGH);
+    solenoid = true;
+  }
+  else{
+    digitalWrite(LEDA,HIGH);
+    digitalWrite(LEDB,HIGH);
+    solenoid = false;
+  }
+  return solenoid;
+}
