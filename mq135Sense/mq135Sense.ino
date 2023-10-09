d40c6f97df21eb3cfeffb48307d73e3d9db259e5
raythibeault1 <raythibeault1@gmail.com>
2023-10-09 13:59:54 -0400

    created sensor folder to start trying sense readings

---
+++ mq135Sense/mq135Sense.ino
@@ -0,0 +1,15 @@
+//this code is to try using the MQ135 for sensing. It reads values from the sensor and displays them in
+//the serial monitor.
+
+int LED1 = 5;        //LED 1 pin
+int sense1 = A3;     //analog pin for MQ135 sensor
+
+void setup() {
+    pinMode(LED1,OUTPUT);
+    pinMode(sense1,INPUT);
+    
+}
+
+void loop() {
+
+}
