9268f9cbe30c7cf2c929c65b12387c7a2627d410
raythibeault1 <raythibeault1@gmail.com>
2024-02-07 17:13:56 -0500

    succesful UART_code2 for CO2 sensor w/ libraries

--- UART_code2/CO2Sensor.h
+++ UART_code2/CO2Sensor.h
@@ -1,30 +1,36 @@
-#include "Arduino.h"
+
 #ifndef CO2Sensor_h
 #define CO2Sensor_h
-
+#include "Arduino.h"
 
 class CO2Sensor{
   public:
   CO2Sensor(void);
 
   void calibrate();
-
+  /*getPPM writes to the CO2 sensor and waits for a response using getResponse()
+  * @return integer value
+  */
   int getPPM();
 
+  /*getPercent writes to the CO2 sensor using getPPM(), then converts the PPM value to a percent
+  * which it returns
+  */
   float getPercent();
 
   void initialize();
 
-  int getResponse();
+  int getResponse(String command);
 
   int extractNumber(String data);
 
   private:
   int _filterValue;
+  int _pollState;
   int _multiplier;
   int _calibrateValue;
   int _currentPPM;
-  int _currentPercent;
+  float _currentPercent;
   char sensorDataBuffer[256];           //buffer character
   int bufferPosition = 0;               // Variable to keep track of the buffer position.
   String _response;
